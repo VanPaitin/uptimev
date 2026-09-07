f7790b56f611cbb1354c25fc0f65dc74c3c58376
Mayowa Pitan <mayowasolomon@gmail.com>
2026-09-07 12:39:34 +0100

    Lock the Homebrew formula to v0.1.0

--- Formula/uptimev.rb
+++ Formula/uptimev.rb
@@ -2,7 +2,7 @@ class Uptimev < Formula
   desc "Print a friendly summary of how long this machine has been running"
   homepage "https://github.com/VanPaitin/uptimev"
   url "https://github.com/VanPaitin/uptimev/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_SHA256_AFTER_YOU_CREATE_THE_RELEASE_TARBALL"
+  sha256 "fb94ff132df5daccab87061240ffd2b96439fc11697b4d1db7f0d81c311b18bf"
   license "MIT"
 
   def install
@@ -10,7 +10,7 @@ class Uptimev < Formula
   end
 
   test do
-    assert_match "Up for ", shell_output("#{bin}/uptimev")
+    assert_match "Up for ", shell_output(bin/"uptimev")
     assert_match version.to_s, shell_output("#{bin}/uptimev --version")
   end
 end
